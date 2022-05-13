700c26a87e40f0bdd7417be0b21083dc5cfdeddb
Nana kadode <amar.nana5048@gmail.com>
2022-05-13 12:12:49 +0900

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@
 # Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
 require 'open-uri'
-List.destroy_all
+Movie.destroy_all
 # List.destroy_all
 
 # the Le Wagon copy of the API
@@ -20,9 +20,11 @@ url = 'http://tmdb.lewagon.com/movie/top_rated'
 response = JSON.parse(URI.open(url).read)
 
 response['results'].each do |movie_hash|
-
   Movie.create!(
-    title: "https://image.tmdb.org/t/p/w500" + movie_hash['title']
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+    rating: movie_hash['vote_average']
   )
   # create an instance with the hash
   # Movie.create!(
@@ -30,3 +32,5 @@ response['results'].each do |movie_hash|
   #   ...
   # )
 end
+
+puts "Created #{Movie.count} movies."
